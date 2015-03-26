ab198d72bc100c4938243534f4b381c7136cba7e
krallm <krallm@southwestern.edu>
2015-03-26 16:13:45 -0500

    Have server listen for webhook push events securely, and do stuff when the 'live' branch is pushed to

--- server.rb
+++ server.rb
@@ -3,17 +3,33 @@ require 'json'
 require 'sinatra'
 
 # Todo: Handle cases where two pushes are sent almost simultaneously
+# Potential Issue: Executing `sh on_push.sh` blocks, so the HTTP response
+#                  isn't sent until the script completes.
+
+set :bind, '0.0.0.0'
+set :port, 3090
 
 post '/payload' do
+  request.body.rewind
+  payload_body = request.body.read
+  verify_signature(payload_body)
+
   begin
-    push = JSON.parse(request.body.read)
+    push = JSON.parse(payload_body)
   rescue
     puts 'Error parsing \'push\' JSON'
     return
   end
 
-  # Todo: Verify that payload is from GitHub
+  # Only update if 'live' branch was pushed to
+  # Should match branch name in config.sh
+  if push.has_key?('ref') and push['ref'] == 'refs/heads/live'
+    cmd = Cocaine::CommandLine.new('sh', 'on_push.sh')
+    cmd.run
+  end
+end
 
-  cmd = Cocaine::CommandLine.new('sh', 'on_push.sh')
-  cmd.run
+def verify_signature(payload_body)
+  signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), ENV['SECRET_TOKEN'], payload_body)
+  return halt 500, "Signatures didn't match!" unless Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
 end
